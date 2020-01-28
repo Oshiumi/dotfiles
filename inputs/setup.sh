9f90aeb9627405752b6541cd92cdcb51adccf27b
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2020-01-28 14:56:33 +0900

    Fix

--- inputs/setup.sh
+++ inputs/setup.sh
@@ -2,5 +2,6 @@
 set -eux
 listing=$(hdiutil attach ./inputs/ProgrammerDvorak.dmg | grep Volumes)
 volume=$(echo "$listing" | cut -f 3)
+[[ ! -e '/Library/Keyboard Layouts' ]] && mkdir -p '/Library/Keyboard Layouts'
 cp -rf $volume/ProgrammerDvorak.bundle '/Library/Keyboard Layouts/'
 hdiutil detach $volume
