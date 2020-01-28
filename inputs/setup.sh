749424242db8e6c679bafcc2fdc561a6d45bbe8d
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2020-01-28 14:53:35 +0900

    Fix path

--- inputs/setup.sh
+++ inputs/setup.sh
@@ -1,6 +1,6 @@
 [[ -e '/Library/Keyboard Layouts/ProgrammerDvorak.bundle' ]] && exit 0
 set -eux
-listing=$(hdiutil attach ProgrammerDvorak.dmg | grep Volumes)
+listing=$(hdiutil attach ./inputs/ProgrammerDvorak.dmg | grep Volumes)
 volume=$(echo "$listing" | cut -f 3)
 cp -rf $volume/ProgrammerDvorak.bundle '/Library/Keyboard Layouts/'
 hdiutil detach $volume
